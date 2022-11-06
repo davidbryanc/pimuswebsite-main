d2b317f4675789f9c90a3d45ab27b3dd7362f143
Muhammad Ikhsan <iksan8991@gmail.com>
2022-11-06 23:09:20 +0700

    update timeline

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -156,7 +156,7 @@
                 </li>
                 <li>
                     <div class="timeline-content">
-                        <h3 style="color: #ffffff" class="date">18 November 2022</h3>
+                        <h3 style="color: #ffffff" class="date">17-18 November 2022</h3>
                         <h1>Technical Meeting</h1>
                     </div>
                 </li>
@@ -168,7 +168,7 @@
                 </li>
                 <li>
                     <div class="timeline-content">
-                        <h3 style="color: #ffffff" class="date">30 November 2022</h3>
+                        <h3 style="color: #ffffff" class="date">24-30 November 2022</h3>
                         <h1>Pengumpulan Proposal untuk di Revisi (PKM, KTI, dan Pilmapres)</h1>
                     </div>
                 </li>
